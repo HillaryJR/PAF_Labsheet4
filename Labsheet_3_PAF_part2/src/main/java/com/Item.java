b47b380f377b7c23561839b29fb443a9e4a4e642
HillaryJR <hillaryjeramin@gmail.com>
2021-04-14 07:54:34 +0530

    Create a method named connect()

--- Labsheet_3_PAF_part2/src/main/java/com/Item.java
+++ Labsheet_3_PAF_part2/src/main/java/com/Item.java
@@ -1,5 +1,29 @@
 package com;
+import java.sql.*;
 
 public class Item {
 
+/**********************DB Connection****************************/
+	
+	public Connection connect() 
+	{ 
+	 	Connection con = null; 
+	 	 	 
+	        try  
+	        { 
+	         	Class.forName("com.mysql.jdbc.Driver"); 
+	         	con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_2021", 
+	 	 	 	 	 	 	 	 	 	"root", "");    
+	 
+	 	 	//For testing 
+	         	System.out.print("Successfully connected");    
+	        } 
+	        catch(Exception e)  
+	        { 
+	         	e.printStackTrace();          
+	        }        
+	       return con; 
+	} 
+
+	
 }
